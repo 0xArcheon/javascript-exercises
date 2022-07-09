a7f00c0adfd1a2d9d77e8d7062c48c182dcc1861
0xArcheon <imamlan03@gmail.com>
2022-07-09 18:41:54 +0530

    Solved exercises 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function(str , num) {
+    if(num<0) {
+        return "ERROR";
+    }
+    else {
+        let repeatString = "";
+        for(let i = 0; i< num; i++)
+        {
+        repeatString = repeatString+str;
+        }
+        return repeatString;
+    }
+   
+}
 // Do not edit below this line
 module.exports = repeatString;
