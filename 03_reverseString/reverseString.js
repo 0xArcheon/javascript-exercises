79fc5376e8655eb847365e5daee0431e502cb24e
0xArcheon <imamlan03@gmail.com>
2022-08-20 13:17:22 +0530

    Solved problems 3 and 4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(ogString) {
+    let revString = "";
+    for( let i= ogString.length; i>= 0; i--)
+    {
+        revString = revString + ogString.charAt(i).toLowerCase();
+    }
+    return revString;
 };
 
 // Do not edit below this line
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(givenArr, ...delItem) {
+        let deletedArr = givenArr.filter((e) => !delItem.includes(e));
+        return deletedArr;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
